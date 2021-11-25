254df6d98167ec97cac15ca280f2be30bae016e7
XaverWeste <westxave@kkg-do.eu>
2021-11-25 13:31:06 +0100

    AnimatedList Class und AnimatedListInterface Interface hinzugefügt

---
+++ src/my_project/model/AnimatedList.java
@@ -0,0 +1,39 @@
+package my_project.model;
+
+import KAGO_framework.control.ViewController;
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+public class AnimatedList <T extends GraphicalObject & AnimatedListInterface>{
+
+    private ListBall previous;
+    private ListBall next=null;
+    private boolean isOnPointer;
+    private boolean arrived;
+    private boolean deleted;
+    private ViewController viewController;
+
+    public AnimatedList(double x,ListBall previousBall, ViewController viewController){
+        previous=previousBall;
+        this.viewController=viewController;
+        isOnPointer=false;
+        arrived = false;
+        deleted = false;
+        //viewController.draw(this);
+    }
+
+    public void draw(DrawTool drawTool){
+
+    }
+
+    public void setPrevious(ListBall newPrevious){ previous=newPrevious; }
+    public void setNext(ListBall theNext){ next=theNext; }
+    public ListBall getPrevious(){ return previous; }
+    public boolean tryToDelete(){
+        return deleted=true;
+    }
+
+    public void update(double dt){
+
+    }
+}
---
+++ src/my_project/model/AnimatedListInterface.java
@@ -0,0 +1,11 @@
+package my_project.model;
+
+import KAGO_framework.view.DrawTool;
+
+public interface AnimatedListInterface {
+
+    public void draw(DrawTool drawTool);
+
+    public void update(double dt);
+
+}
