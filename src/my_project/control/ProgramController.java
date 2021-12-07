c04b43b5f1aab3db250dd30b04965f1082b379ae
westxave <westxave@kkg-do.eu>
2021-12-07 11:14:18 +0100

    zweite List hinzugefügt

--- src/my_project/control/ProgramController.java
+++ src/my_project/control/ProgramController.java
@@ -2,6 +2,7 @@ package my_project.control;
 
 import KAGO_framework.control.ViewController;
 import my_project.model.AnimatedList;
+import my_project.model.ListBall;
 import my_project.model.ListCircle;
 import my_project.view.InputReceiver;
 
@@ -11,6 +12,7 @@ public class ProgramController {
 
     private final ViewController viewController;
     private AnimatedList<ListCircle> list;
+    private AnimatedList<ListBall> lisst;
 
     public ProgramController(ViewController viewController){
         this.viewController = viewController;
@@ -19,6 +21,7 @@ public class ProgramController {
     public void startProgram() {
         new InputReceiver(this,viewController);
         list = new AnimatedList<>();
+        lisst= new AnimatedList<>();
     }
 
     public void addBall(String to){
@@ -36,6 +39,23 @@ public class ProgramController {
 
     public void changeListPointer(String to){
         list.changeListPointer(to);
+    }
+
+    public void adBall(String to){
+        if (lisst.getlast() != null) {
+            lisst.addToList(to, new ListBall(1000,lisst.getlast(), viewController));
+        } else {
+            lisst.addToList(to, new ListBall(1000,null, viewController));
+            lisst.getList().toFirst();
+        }
+    }
+
+    public void deletteBall(){
+        lisst.deleteFromList();
+    }
+
+    public void changeLisstPointer(String to){
+        lisst.changeListPointer(to);
     }
 
     public void mouseClicked(MouseEvent e){
